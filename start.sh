0631ea596e86b831fb2a2eaeb49f3ec6ac7b60bb
Raymond Mwebe <122720713+Raymondcoder27@users.noreply.github.com>
2024-08-19 17:06:09 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,3 +11,21 @@ fi
 # nginx -g 'daemon off;'
 # Serve frontend using serve
 serve -s /usr/share/nginx/html -l 80
+
+
+
+#!/bin/sh
+
+# Export environment variables from .env file
+# if [ -f /usr/local/bin/.env ]; then
+  # export $(grep -v '^#' /usr/local/bin/.env | xargs)
+# fi
+
+# Start the backend application
+# /usr/local/bin/main &
+
+# Start serving the frontend on port 4000
+# serve -s /usr/share/nginx/html -l 4000 &
+
+# Wait indefinitely to keep the container running
+# wait
