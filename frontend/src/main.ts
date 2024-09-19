07528c1013bf8da5c5aba41b82dc589fc9ee033b
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-19 13:17:22 +0300

    adds json payload saving

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,10 @@ import "@fortawesome/fontawesome-free/css/all.min.css"
 import App from './App.vue'
 import { createPinia } from "pinia"
 import router from "./router";
+import CanvasJSChart from '@canvasjs/vue-chartjs'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(CanvasJSChart)
 app.use(router)
 app.mount('#app')
