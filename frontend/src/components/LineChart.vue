65ede9de7efef3f78e09a975a8a16a98e5ffddea
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-20 13:12:38 +0300

    configures dashbaord chart

--- frontend/src/components/LineChart.vue
+++ frontend/src/components/LineChart.vue
@@ -154,7 +154,7 @@ async function fetchMetrics() {
 
 async function fetchDocumentHistory() {
     try {
-        const response = await axios.get('http://localhost:4100/document-history');
+        const response = await axios.get('http://localhost:8080/document-history');
         if (response.status !== 200) {
             throw new Error('Failed to fetch document history');
         }
