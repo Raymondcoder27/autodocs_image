99a445f10e3933df7fbb3ffb1d4f0d5f10ec5a74
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-20 17:48:53 +0300

    configures dashbaord chart

--- frontend/src/domain/requests/stores/index.ts
+++ frontend/src/domain/requests/stores/index.ts
@@ -7,45 +7,17 @@ import api from "@/config/api";
 
 export const useDocumentStore = defineStore("documents", () => {
 
-    const documents: Ref<Doc[] | undefined> = ref()
-    const fileBase64: Ref<string | undefined> = ref()
-    const generationResponse: Ref<object | undefined> = ref()
+    const logs: Ref<Doc[] | undefined> = ref()
 
-    const fetchDocuments = async () => {
-        return api.get("/documents")
+    const fetchLogs = async () => {
+        return api.get("/logs")
             .then((response: AxiosResponse<ApiResponse<Doc[]>>) => {
-                documents.value = response.data.data
+                logs.value = response.data.data
             })
     }
 
-    const fetchDocumentFile = async (ref:string) => {
-        return api.get("/documents/preview/"+ref)
-            .then((response: AxiosResponse<ApiResponse<string>>) => {
-                fileBase64.value = response.data.data
-            })
-    }
-
-    const sendRequest = async (payload: GenerationRequest) => {
-        return api.post("/generate", payload)
-            .then((response: AxiosResponse<ApiResponse<any>>) => {
-                generationResponse.value = response.data
-            })
-    }
-
-    const deleteDocument = async (ref:string) => {
-        return api.delete("/documents/"+ref)
-            .then((response: AxiosResponse<ApiResponse<any>>) => {
-                generationResponse.value = response.data
-            })
-    }
 
     return {
-        documents,
-        generationResponse,
-        fileBase64,
-        fetchDocuments,
-        fetchDocumentFile,
-        deleteDocument,
-        sendRequest
+        fetchLogs,
     }
 })
