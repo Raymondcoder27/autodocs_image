521158e2167cc27fbd1a753568a8ff2550d502d6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-21 16:12:59 +0300

    fixes frontend requests page bugs

--- frontend/src/domain/requests/Requests.vue
+++ frontend/src/domain/requests/Requests.vue
@@ -319,12 +319,11 @@ function downloadPdf() {
 
 // const requestLogs: Ref<{ method: string, status: string }[]> = ref([]);
 
-
 //paginating logs using the same logic as documents
 const paginatedLogs = computed(() => {
   const start = (currentPage.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
-    return logStore.logs.slice(start, end);
+  return logStore.logs.slice(start, end);
 });
 
 function nextPage() {
--- frontend/src/domain/requests/stores/index.ts
+++ frontend/src/domain/requests/stores/index.ts
@@ -1,12 +1,13 @@
 import {defineStore} from "pinia";
 import type {AxiosResponse} from "axios";
 import type {ApiResponse} from "@/types";
+import type {Log} from "@/domain/requests/types";
 import {ref, type Ref} from "vue";
 import api from "@/config/api";
 
 export const useLogStore = defineStore("logs", () => {
 
-    const logs: Ref<Doc[] | undefined> = ref()
+    const logs: Ref<Log[] | undefined> = ref()
 
     const fetchLogs = async () => {
         return api.get("/logs")
