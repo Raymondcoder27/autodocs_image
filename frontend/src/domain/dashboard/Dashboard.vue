58f0bb2803acfb600bdd076bbe8a3f59596a7148
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-23 15:28:36 +0300

    fixes application pagination bugs

--- frontend/src/domain/dashboard/Dashboard.vue
+++ frontend/src/domain/dashboard/Dashboard.vue
@@ -87,6 +87,10 @@ async function fetchChartData() {
 
 <template>
     <div class="p-0">
+        <div class="mb-3">
+            <DatePicker v-model="startDate" label="Start Date" id="start-date" />
+            <DatePicker v-model="endDate" label="End Date" id="end-date" />
+        </div>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-3 mb-3">
             <div class="bg-white border border-blue-100 p-5 rounded-lg shadow text-center">
                 <p class="text-3xl font-bold text-blue-700">{{ totalTemplates }}</p>
