e1940bd2bdf4a4b48d86a9e7ffaf8af8a3a65a50
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-23 15:17:46 +0300

    fixes vue chart bugs

--- frontend/src/domain/dashboard/Dashboard.vue
+++ frontend/src/domain/dashboard/Dashboard.vue
@@ -105,7 +105,7 @@ async function fetchChartData() {
                 <h3 class="text-lg font-semibold text-red-300">Failed Generations</h3>
             </div>
             <div class="bg-white border border-gray-400 p-5 rounded-lg shadow text-center">
-                <p class="text-3xl font-bold text-gray-600">{{ generationRate.toFixed(2) }}</p>
+                <p class="text-3xl font-bold text-gray-600">{{ generationRate.toFixed(2) }}%</p>
                 <h3 class="text-lg font-semibold text-gray-500">Generation Rate (daily)</h3>
             </div>
             <div class="bg-white border border-blue-100 p-5 rounded-lg shadow text-center">
