b49c19a141b85674f688bc09d0a53d622f470490
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-23 12:15:45 +0300

    fixes application pagination bugs

--- frontend/src/domain/documents/Documents.vue
+++ frontend/src/domain/documents/Documents.vue
@@ -114,6 +114,13 @@ function downloadPdf() {
 //   return store.documents.slice(start, end);
 // });
 
+// const paginatedDocuments = computed(() => {
+//   const documents = store.documents || []; // Ensure `documents` is an array
+//   const start = (currentPage.value - 1) * itemsPerPage;
+//   const end = start + itemsPerPage;
+//   return documents.slice(start, end);
+// });
+
 const paginatedDocuments = computed(() => {
   const documents = store.documents || []; // Ensure `documents` is an array
   const start = (currentPage.value - 1) * itemsPerPage;
