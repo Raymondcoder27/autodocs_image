c5b694dce16b019357dc86894edf5f2a3f14a298
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-23 15:31:39 +0300

    fixes application pagination bugs

---
+++ frontend/src/components/DatePicker.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <label :for="id">{{ label }}</label>
+      <input :id="id" type="date" v-model="dateValue" @input="updateDate" />
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import { defineProps, defineEmits, ref, watch } from 'vue';
+  
+  const props = defineProps({
+    modelValue: {
+      type: String,
+      required: true
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    id: {
+      type: String,
+      required: true
+    }
+  });
+  
+  const emits = defineEmits(['update:modelValue']);
+  
+  const dateValue = ref(props.modelValue);
+  
+  watch(dateValue, (newValue) => {
+    emits('update:modelValue', newValue);
+  });
+  
+  function updateDate(event: Event) {
+    const target = event.target as HTMLInputElement;
+    dateValue.value = target.value;
+  }
+  </script>
+  
+  <style scoped>
+  /* Add any styles you need for the date picker */
+  </style>
