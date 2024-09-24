60eb35ace82d9c8066e44dfdd40446aaf12c7ae6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-24 15:04:16 +0300

    fixes vue chart bugs

--- frontend/src/domain/dashboard/Dashboard.vue
+++ frontend/src/domain/dashboard/Dashboard.vue
@@ -99,9 +99,9 @@ async function fetchChartData() {
 
 <template>
     <div class="p-0">
-        <div class="mb-3 bg-white font-semibold text-gray-500 rounded-md max-w-[350px] p-1 justify-around">
+        <div class="mb-3 bg-white font-semibold text-gray-500 rounded-md max-w-[350px] p-1 mx-auto">
             <div class="text-semibold text-black-900 text-xs text-center">CHOOSE DATES TO VIEW REPORT.</div>
-            <div class="flex text-xs justify-between">
+            <div class="flex text-xs ml-[35px]">
                 <DatePicker v-model="startDate" label="START DATE:  " id="start-date" />
             <DatePicker v-model="endDate" label="END DATE:  " id="end-date" />
             </div>
